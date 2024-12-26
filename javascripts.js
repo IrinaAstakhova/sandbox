b247dab4e70798958f51c5e7e06f7c6f43797a86
Irina Astakhova <aiv0601@gmail.com>
2024-12-26 20:12:47 +0300

    Добавление функционала для кнопки задача выполнена

--- javascripts.js
+++ javascripts.js
@@ -6,11 +6,20 @@ const inputToDo = document.querySelector('[data-input]');
 const buttonTodo = document.querySelector('#addtodo');
 const tasksToDo = document.querySelector('.tasks');
 
-const toDoList = JSON.parse(localStorage.getItem("todos")) || [];
+// const toDoList = JSON.parse(localStorage.getItem("todos")) || [];
 
-function saveToLocalStorage(key = "todos") {
-    localStorage.setItem(key, JSON.stringify(toDoList));
-    }
+
+
+function saveToLocalStorage(todos) {
+    localStorage.setItem("todos", JSON.stringify(todos));
+    };
+
+const getToLocalStorage = () => {
+      return JSON.parse(localStorage.getItem("todos")) || [];
+      };
+
+const toDoList = getToLocalStorage ();
+
 
 //Шаблон для прорисовки
 
@@ -35,11 +44,12 @@ function delBtn() {
     button[i].addEventListener('click', () => {
       log("нажата кнопка удалить");
       toDoList.splice([i], 1);
-      saveToLocalStorage();
+      saveToLocalStorage(toDoList);
       render();
     });
   };
 };
+
 
 
 //Добавляем стиль выполненной задачи
@@ -49,19 +59,28 @@ function doneBtn() {
     for (let j = 0; j < button.length; j++) {
       button[j].addEventListener('click', () => {
           item[j].classList.toggle('done-task');
+          toDoList[j].completed = !(toDoList[j].completed);
+          saveToLocalStorage(toDoList);
+
       });
 };
 };
+
+
   
   
  //Ожидание и создание задачи 
   buttonTodo.addEventListener("click", () => {
       if (!(inputToDo.value.trim() === '')) {
-          toDoList.push(inputToDo.value);
-          addBlockHtml(inputToDo.value);
+        const newTodo = {
+          id: Date.now(),
+          text: inputToDo.value.trim(),
+          completed: false,
+          };
+          toDoList.push(newTodo);
       inputToDo.value = '';
       render ();
-      saveToLocalStorage();
+      saveToLocalStorage(toDoList);
   } else {
       alert('Поле не может быть пустым')
   };
@@ -71,12 +90,25 @@ function doneBtn() {
   //Прорисовка из массива
   function render () {
       tasksToDo.innerHTML = '';
-      toDoList.forEach(todo => {
+      const todoText = toDoList.map(({ text }) => text);
+      todoText.forEach(todo => {
       addBlockHtml(todo); 
+      
     });
+    const tasks = document.querySelectorAll('.task');
+    tasks.forEach(task => {
+      task.classList.remove('done-task');
+    })
+    
+    for (let i = 0; i < toDoList.length; i++) {
+      if (toDoList[i].completed === true) {
+        tasks[i].classList.add('done-task');
+      }
+    }
 
     delBtn();
     doneBtn();
+
   };
 
 

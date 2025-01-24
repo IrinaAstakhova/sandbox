1d9fd8aba4df08e1753528000b09797ee6c5a5b6
Irina Astakhova <aiv0601@gmail.com>
2025-01-24 15:04:07 +0300

    Добавление анимации к слайдеру, упрощение кода

--- slider.js
+++ slider.js
@@ -1,22 +1,34 @@
 const images =[
     "./slides/1.jpg", "./slides/2.jpg", "./slides/3.jpg", "./slides/4.jpg", "./slides/5.jpg", "./slides/6.jpg", "./slides/7.jpg"
 ];
-
+let i = 1;
 const slider = document.querySelector("[data-slider]");
 const btnPrev = document.querySelector("[data-btn-prev]");
 const btnNext = document.querySelector("[data-btn-next]");
 
-const setupSlides = () => {
-    images.forEach((imageUrl, index) => {
-    const img = document.createElement("img");
-    img.src = imageUrl;
-    img.dataset.index = index;
-    img.alt = `slide ${index + 1}`;
 
-    slider.appendChild(img);
-    });
-
-    
+const outputSlides = () => {
+    slider.innerHTML =`<img src=${images[i-1]} alt=slide_${images.indexOf(images[i-1]) + 1}>`;
+    log(images[i-1])
 }
 
-setupSlides();
+outputSlides();
+
+btnNext.onclick = () =>{
+    if(i < images.length){
+        i++;
+    }else{
+        i = 1;
+    }
+
+    outputSlides();
+}
+
+btnPrev.onclick = () =>{
+    if(i > 1 ){
+        i--;
+    }else{
+        i = images.length;
+    }
+    outputSlides();
+}
